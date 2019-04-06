abe07142668f958687e5f8d1b9be12325e5ec61b
Hannes Mehnert <hannes@mehnert.org>
2019-04-07 01:25:13 +0200

    secondary

--- secondary/config.ml
+++ secondary/config.ml
@@ -2,6 +2,10 @@
 
 open Mirage
 
+let net = generic_stackv4 default_network
+
+let logger = syslog_udp ~config:(syslog_config "sn.nqsb.io") net
+
 let keys =
   let doc = Key.Arg.info ~doc:"nsupdate keys (name:type:value,...)" ["keys"] in
   Key.(create "keys" Arg.(opt (list string) [] doc))
@@ -21,10 +25,10 @@ let dns_handler =
   and keys = Key.([ abstract keys ])
   in
   foreign
-    ~deps:[abstract nocrypto]
+    ~deps:[abstract nocrypto ; abstract logger ; abstract app_info]
     ~keys
     ~packages
     "Unikernel.Main" (random @-> pclock @-> mclock @-> time @-> stackv4 @-> job)
 
 let () =
-  register "secondary" [dns_handler $ default_random $ default_posix_clock $ default_monotonic_clock $ default_time $ generic_stackv4 default_network ]
+  register "snnqsb" [dns_handler $ default_random $ default_posix_clock $ default_monotonic_clock $ default_time $ generic_stackv4 default_network ]
