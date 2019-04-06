e69acbe944906ed92f9ca3816cf598a67c5c4416
Hannes Mehnert <hannes@mehnert.org>
2019-04-07 01:25:13 +0200

    secondary

--- secondary/unikernel.ml
+++ secondary/unikernel.ml
@@ -7,7 +7,12 @@ open Mirage_types_lwt
 module Main (R : RANDOM) (P : PCLOCK) (M : MCLOCK) (T : TIME) (S : STACKV4) = struct
   module D = Udns_mirage_server.Make(P)(M)(T)(S)
 
-  let start _rng pclock mclock _ s _ =
+  let start _rng pclock mclock _ s _ _ info =
+    Logs.info (fun m -> m "used packages: %a"
+                  Fmt.(Dump.list @@ pair ~sep:(unit ".") string string)
+                  info.Mirage_info.packages) ;
+    Logs.info (fun m -> m "used libraries: %a"
+                  Fmt.(Dump.list string) info.Mirage_info.libraries) ;
     let keys = List.fold_left (fun acc str ->
         match Udns.Dnskey.name_key_of_string str with
         | Error (`Msg msg) -> Logs.err (fun m -> m "key parse error: %s" msg) ; acc
