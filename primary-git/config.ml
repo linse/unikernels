793a04c1c0791c69471a0e3cac1290bf89f23d2b
Hannes Mehnert <hannes@mehnert.org>
2019-04-07 00:51:55 +0200

    deploy

--- primary-git/config.ml
+++ primary-git/config.ml
@@ -2,6 +2,10 @@
 
 open Mirage
 
+let net = generic_stackv4 default_network
+
+let logger = syslog_udp ~config:(syslog_config "ns.nqsb.io") net
+
 let keys =
   let doc = Key.Arg.info ~doc:"nsupdate keys (name:type:value,...)" ["keys"] in
   Key.(create "keys" Arg.(opt (list string) [] doc))
@@ -11,9 +15,9 @@ let remote_k =
   Key.(create "remote" Arg.(opt string "https://github.com/roburio/udns.git" doc))
 
 let dns_handler =
-  let udns_pin = "git+https://github.com/roburio/udns.git" in
-  let irmin_pin = "git+https://github.com/hannesm/irmin.git#minor" in
-  let git_pin = "git+https://github.com/hannesm/ocaml-git.git#minor" in
+  let udns_pin = "git+https://github.com/roburio/udns.git#nsnqsb" in
+  let irmin_pin = "git+https://github.com/hannesm/irmin.git#nsnqsb" in
+  let git_pin = "git+https://github.com/hannesm/ocaml-git.git#nsnqsb" in
   let packages = [
     package "logs" ;
     package ~pin:udns_pin "udns";
@@ -27,25 +31,23 @@ let dns_handler =
     package "nocrypto" ;
     package ~pin:irmin_pin "irmin";
     package ~pin:irmin_pin "irmin-git";
-    package ~pin:irmin_pin "irmin-graphql";
     package ~pin:irmin_pin "irmin-mem";
     package ~pin:irmin_pin "irmin-mirage";
-    package ~pin:"git+https://github.com/hannesm/encore.git#minor" "encore";
+    package ~pin:"git+https://github.com/hannesm/encore.git#nsnqsb" "encore";
     package ~pin:git_pin "git";
     package ~pin:git_pin "git-http";
     package ~pin:git_pin "git-mirage";
-    package ~pin:"git+https://github.com/hannesm/ocaml-conduit.git#udns" "mirage-conduit";
-    package ~pin:"git+https://github.com/mirage/ke.git" "ke"
+    package ~pin:"git+https://github.com/hannesm/ocaml-conduit.git#nsnqsb" "mirage-conduit";
+    package ~pin:"git+https://github.com/hannesm/ke.git#nsnqsb" "ke"
   ] in
   foreign
-    ~deps:[abstract nocrypto]
+    ~deps:[abstract nocrypto; abstract logger ; abstract app_info]
     ~keys:[Key.abstract remote_k ; Key.abstract keys]
     ~packages
     "Unikernel.Main"
     (random @-> pclock @-> mclock @-> time @-> stackv4 @-> resolver @-> conduit @-> job)
 
 let () =
-  let net = generic_stackv4 default_network in
-  register "primary-git"
+  register "nsnqsb"
     [dns_handler $ default_random $ default_posix_clock $ default_monotonic_clock $
      default_time $ net $ resolver_dns net $ conduit_direct ~tls:true net]
